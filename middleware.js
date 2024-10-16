88ca4ae2aea86495716b01a15fcc78172f86b604
Bruno Tejeria <brunotejeria@hotmail.com>
2024-10-16 20:35:15 -0300

    pito pito gorgorito

--- middleware.js
+++ middleware.js
@@ -3,12 +3,12 @@ import { next } from '@vercel/edge';
 export default function middleware(req) {
   return next({
     headers: {
-      'Referrer-Policy': 'origin-when-cross-origin',
-      'X-Frame-Options': 'DENY',
-      'X-Content-Type-Options': 'nosniff',
+      'Referrer-Policy': 'no-referrer-when-downgrade', // Permite más flexibilidad en el envío de la URL de referencia
+      'X-Frame-Options': 'SAMEORIGIN', // Permite que tu sitio se incruste dentro de iframes en el mismo origen
+      'X-Content-Type-Options': 'nosniff', // Mantiene la seguridad MIME, pero esto no debería afectar imágenes
       'X-DNS-Prefetch-Control': 'on',
       'Strict-Transport-Security':
-        'max-age=31536000; includeSubDomains; preload',
+        'max-age=31536000; includeSubDomains; preload', // Mantiene la seguridad HTTPS
     },
   });
 }
